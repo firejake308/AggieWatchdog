c959c41b2c4c569af004e5c5708992b3f24931bb
Adel Hassan <sean.dashals@gmail.com>
2019-10-31 13:21:28 -0500

    Renders header

--- App.tsx
+++ App.tsx
@@ -1,11 +1,43 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, Platform, StatusBar } from 'react-native';
+import { Root, Container, Header, Left, Right, Title, Icon } from 'native-base';
+import { AppLoading } from 'expo';
+import * as Font from 'expo-font';
 
 export default function App() {
+  const [loading, setLoading] =  useState(true);
+  
+  useEffect(() => {
+    async function loadFont() {
+      await Font.loadAsync({
+        Roboto: require('native-base/Fonts/Roboto.ttf'),
+        Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
+      })
+      setLoading(false);
+    }
+    loadFont(); 
+  }, [loading]);
+
+  if (loading)
+    return (
+      <Root>
+        <AppLoading />
+      </Root>
+    )
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+    <Root>
+      <Container>
+        <Header style={styles.customHeader}>
+          <Left>
+            <Title>PingMe</Title>
+          </Left>
+          <Right>
+            <Icon name="save" color="white" />
+          </Right>
+        </Header>
+      </Container>
+    </Root>
   );
 }
 
@@ -16,4 +48,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  customHeader: {
+    paddingTop: StatusBar.currentHeight,
+    height: 54 + StatusBar.currentHeight
+  }
 });
